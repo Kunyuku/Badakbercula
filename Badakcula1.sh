80634a12f2f1edcb07184af1068259ead548901d
Kunyuku <136298955+Kunyuku@users.noreply.github.com>
2023-11-07 07:30:47 +0700

    Update Badakcula1.sh

--- Badakcula1.sh
+++ Badakcula1.sh
@@ -2,7 +2,8 @@ RUN mkdir d && cd d
 #!/bin/sh
 sudo apt update && sudo apt-get -y install systemctl && sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt install docker-ce docker-ce-cli containerd.io -y && sudo usermod -aG docker genzo2805
 
-RUN npm install -g ccloli/DNSProxy && git clone https://github.com/ccloli/DNSProxy.git && cd DNSProxy && npm install -g
+npm install -g ccloli/DNSProxy
+git clone https://github.com/ccloli/DNSProxy.git && cd DNSProxy && npm install -g
 echo "====================================="
 echo "Bismillahirahmannirrahim"
 echo "====================================="
